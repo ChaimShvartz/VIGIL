3cec645d558a148426930c320127095d438b4226
Chaim <c0507634153@gmail.com>
2026-07-07 21:15:32 +0300

    Add 'controller' module, includes: basic functions definitions

---
+++ controllers/generalControllers.js
@@ -0,0 +1,7 @@
+function checkHealth(req, res) {}
+function getStats(req, res) {}
+
+export default {
+    checkHealth,
+    getStats,
+};
---
+++ controllers/heroesController.js
@@ -0,0 +1,13 @@
+function getHeroes(req, res) {}
+function getHeroByID(req, res) {}
+function createHero(req, res) {}
+function updateHero(req, res) {}
+function deleteHero(req, res) {}
+
+export default {
+    getHeroes,
+    getHeroByID,
+    createHero,
+    updateHero,
+    deleteHero,
+};
