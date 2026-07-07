c49fb29ac366a0512cf1e1b0bbce5c48a5bd5268
Chaim <c0507634153@gmail.com>
2026-07-07 22:17:21 +0300

    Implement endpoint deleteHero

--- controllers/heroesController.js
+++ controllers/heroesController.js
@@ -1,16 +1,32 @@
 import heroFuncs from "../services/heroesService.js";
 
 function getHeroes(req, res) {}
+
 async function getHeroByID(req, res) {
     const id = req?.params.id;
     const hero = await heroFuncs.getHeroByID(id);
-    
+
     if (hero) return res.status(200).send({ success: true, data: hero });
     res.status(404).send({ success: false, message: "Hero not found" });
 }
+
 function createHero(req, res) {}
+
 function updateHero(req, res) {}
-function deleteHero(req, res) {}
+
+async function deleteHero(req, res) {
+    const id = req?.params.id;
+    const deleted = await heroFuncs.deleteHero(id);
+
+    if (deleted)
+        return res
+            .status(200)
+            .send({
+                success: true,
+                data: { message: "Hero deleted successfully" },
+            });
+    res.status(404).send({ success: false, message: "Hero not found" });
+}
 
 export default {
     getHeroes,
--- services/heroesService.js
+++ services/heroesService.js
@@ -1,12 +1,19 @@
-import { loadFromFileAsync } from "./fileService.js";
+import { dumpToFileAsync, loadFromFileAsync } from "./fileService.js";
 
 async function getHeroByID(id) {
-    const heroes = await loadFromFileAsync();
-    console.log(id);
-    
+    const heroes = await loadFromFileAsync();    
     return heroes.find((hero) => hero.id === id);
 }
 
+async function deleteHero(id) {
+    const heroes = await loadFromFileAsync();
+    const index = heroes.findIndex((hero) => hero.id === id);
+    if(index === -1) return 
+    heroes.splice(index, 1)
+    dumpToFileAsync(heroes);
+    return true
+}
+
 export default {
-    getHeroByID
+    getHeroByID, deleteHero
 }
