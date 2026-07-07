122ae90b074ad9a7b065878c6b068a827a97ada6
Chaim <c0507634153@gmail.com>
2026-07-07 21:18:44 +0300

    Add 'server' file, includes: basic server

---
+++ server.js
@@ -0,0 +1,50 @@
+import http from "http";
+import routes from "./routes.js";
+import { send, status } from "./services/utils.js";
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+    const urlObj = new URL(req.url, "http://" + req.headers.host);
+
+    const { method } = req;
+    let url = urlObj.pathname;
+    req.queries = Object.fromEntries(urlObj.searchParams);
+
+    if (["GET", "DELETE"].includes(method)) req.resume();
+
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    req.on("end", () => {
+        req.body = body ? JSON.parse(body) : null;
+        res.status = status;
+        res.send = send;
+
+        //  happy path
+        let handler = routes[method]?.[url];
+        if (handler) return handler(req, res);
+
+        //  for path params
+        const lastSlaדh = url.lastIndexOf("/");
+        const param = url.slice(lastSlaדh + 1);
+        const cleanPath = url.slice(0, lastSlaדh);
+
+        //  path + path params
+        for (const curUrl in routes[method]) {
+            if (curUrl.startsWith(cleanPath + "/:")) {
+                const paramName = curUrl.split(":")[1];
+                req.params = { [paramName]: param };
+                return routes[method][curUrl](req, res);
+            }
+        }
+
+        res.status(404).send("Page not found");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000...");
+});
